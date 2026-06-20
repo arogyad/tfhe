1a6e5b552aae4b1f47c036b4de9d5d0f67eb506b
arogya <arogyadahal2003@gmail.com>
2026-06-20 12:21:43 -0400

    Pointwise mult in ntt -> intt

---
+++ sv/pointwise_mult.sv
@@ -0,0 +1,57 @@
+module pointwise_mult import tfhe_pkg::*; (
+    input logic clk,
+    input logic rst,
+    input logic start,
+    input logic [WORD_SIZE - 1:0] in_a [0:N - 1], // NTT
+    input logic [WORD_SIZE - 1:0] in_b [0:N - 1], // NTT
+    output logic done,
+    output logic [WORD_SIZE - 1:0] out_data [0:N - 1]
+);
+    typedef enum logic [1:0] {IDLE, RUN, FINISH} state_t;
+    state_t state;
+
+    logic [LOG_N - 1: 0] i;
+
+    logic [WORD_SIZE - 1: 0] mult_out;
+
+    mod_alu alu_unit (
+        .val1(in_a[i]),
+        .val2(in_b[i]),
+        .out_mul(mult_out)
+    );
+
+    always_ff @( posedge clk  or posedge rst) begin
+
+        if (rst) begin
+            state <= IDLE;
+            done <= 1'b0;
+            i <= 0;
+        end else begin
+            case (state)             
+                IDLE: begin
+                    done <= 1'b0;
+                    i <= 0;
+                    if (start) state <= RUN;
+                end
+
+                RUN: begin
+                    out_data[i] <= mult_out;
+                    if(i == LOG_N'(N - 1)) begin
+                        state <= FINISH;
+                    end else begin
+                        i <= i + 1;
+                    end
+                end
+
+                FINISH: begin
+                    done <= 1'b1;
+                    state <= IDLE;
+                end
+
+                default: state <= IDLE;
+            endcase
+        end
+        
+    end
+
+endmodule
--- sv/test_ntt_mul.sv
+++ sv/test_ntt_mul.sv
@@ -1,4 +1,4 @@
-module test;
+module test_ntt_mul;
 
     import tfhe_pkg::*;
 
@@ -6,28 +6,54 @@ module test;
     logic rst;
 
     logic ntt_start;
-    logic ntt_done;
+    logic ntt_done_1;
+    logic ntt_done_2;
     logic [WORD_SIZE - 1:0] in_data [0:N - 1];
-    logic [WORD_SIZE - 1:0] ntt_out [0:N - 1];
+    logic [WORD_SIZE - 1:0] ntt_out_1 [0:N - 1];
+    logic [WORD_SIZE - 1:0] ntt_out_2 [0:N - 1];
+
+    logic mul_start;
+    logic mul_done;
+    logic [WORD_SIZE - 1:0] mul_out [0:N - 1];
 
     logic intt_start;
     logic intt_done;
     logic [WORD_SIZE - 1:0] intt_out [0:N - 1];
 
-    ntt_top dut_ntt (
+    ntt_top dut_ntt_1 (
         .clk(clk),
         .rst(rst),
         .start(ntt_start),
         .in_data(in_data),
-        .done(ntt_done),
-        .out_data(ntt_out)
+        .done(ntt_done_1),
+        .out_data(ntt_out_1)
+    );
+
+    ntt_top dut_ntt_2 (
+        .clk(clk),
+        .rst(rst),
+        .start(ntt_start),
+        .in_data(in_data),
+        .done(ntt_done_2),
+        .out_data(ntt_out_2)
+
+    );
+
+    pointwise_mult dut_mul(
+        .clk(clk),
+        .rst(rst),
+        .start(mul_start),
+        .in_a(ntt_out_1),
+        .in_b(ntt_out_2),
+        .done(mul_done),
+        .out_data(mul_out)
     );
 
     intt_top dut_intt (
         .clk(clk),
         .rst(rst),
         .start(intt_start),
-        .in_data(ntt_out),
+        .in_data(mul_out),
         .done(intt_done),
         .out_data(intt_out)
     );
@@ -39,8 +65,11 @@ module test;
 
     initial begin
         for (int i = 0; i < N; i++) begin
-            in_data[i] = WORD_SIZE'(i);
+            // in_data[i] = WORD_SIZE'(0);
+            in_data[i] = 0;
         end
+        in_data[0] = 1;
+        in_data[1] = 2; // I am taking polynomials (1 + 2x) . (1 + 2x) so answer should be 1 + 4x + 4x^2
 
         rst = 1;
         ntt_start = 0;
@@ -54,8 +83,15 @@ module test;
         #10;
         ntt_start = 0;
 
-        wait(ntt_done == 1'b1);
+        wait(ntt_done_1 == 1'b1 && ntt_done_2 == 1'b1);
         #10; 
+
+        mul_start = 1;
+        #10;
+        mul_start = 0;
+
+        wait(mul_done == 1'b1);
+        #10;
 
         intt_start = 1;
         #10;
@@ -71,12 +107,7 @@ module test;
             prod = 128'(intt_out[i]) * 128'(64'hEFFFFFFF10000001); // N^-1
             final_val = WORD_SIZE'(prod % 128'(q));
 
-            if (final_val !== in_data[i]) begin
-                $display("index %0d: got %0h expected %0h", i, final_val, in_data[i]);
-                $fatal;
-            end else begin
-                $display("out[%0d] = %0h", i, final_val);
-            end
+            $display("out[%0d] = %0h", i, final_val); // [1, 4, 4, 0, ..., 0] so (1 + 4x + 4x^2) matches
         end
 
         $finish;
